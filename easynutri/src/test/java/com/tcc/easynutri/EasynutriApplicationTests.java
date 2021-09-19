31ca6dd95f667b2266f6396ee0abd008a61f232e
marlonsants <marlutcu@gmail.com>
2021-09-19 11:09:14 -0300

    Commit inicial do projeto

---
+++ easynutri/src/test/java/com/tcc/easynutri/EasynutriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.easynutri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasynutriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
