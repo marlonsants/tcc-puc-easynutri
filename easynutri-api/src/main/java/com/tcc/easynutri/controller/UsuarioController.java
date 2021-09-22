7d21534c03449b197e80df2a9fb4825ccf799cfe
marlon <marlutcu@gmail.com>
2021-09-21 23:36:52 -0300

    Ajuste no controller de usuário

--- easynutri-api/src/main/java/com/tcc/easynutri/controller/UsuarioController.java
+++ easynutri-api/src/main/java/com/tcc/easynutri/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class UsuarioController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Usuario adicionar(Usuario usuario) {
+	public Usuario adicionar(@RequestBody Usuario usuario) {
 		return usuarioRepository.save(usuario);
 	}
 }
