6dcf4fb020d13aea3ec3d6a86b1526f386b8da9a
marlon <marlutcu@gmail.com>
2021-09-22 22:47:06 -0300

    implementação inicial do crud de usuário

--- easynutri-api/src/main/java/com/tcc/easynutri/controller/UsuarioController.java
+++ easynutri-api/src/main/java/com/tcc/easynutri/controller/UsuarioController.java
@@ -2,10 +2,15 @@ package com.tcc.easynutri.controller;
 
 import java.util.List;
 
+import javax.xml.bind.ValidationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,7 +33,38 @@ public class UsuarioController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Usuario adicionar(@RequestBody Usuario usuario) {
-		return usuarioRepository.save(usuario);
+	public void adicionar(@RequestBody Usuario usuario) {
+		usuarioRepository.save(usuario);
 	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void alterar(@PathVariable("id") Long id, @RequestBody Usuario usuario) throws Exception {
+		var usuarioRetornado = usuarioRepository.findById(id);
+		if (usuarioRetornado.isPresent()) {
+			usuarioRetornado.get().setNome(usuario.getNome());
+			usuarioRepository.save(usuario);
+		} else {
+			throw new Exception("O usuário informado não existe");
+		}
+	}
+	
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.OK)
+	public void deletar(@RequestBody Usuario usuario) {
+		usuarioRepository.delete(usuario);
+	}
+	
+	@GetMapping("/{id}")
+	public Usuario buscarPeloId(@PathVariable("id") Long id) throws Exception {
+		var usuario = usuarioRepository.findById(id);
+		
+		if (usuario.isPresent()) {
+			return usuario.get();
+		} else {
+			throw new Exception("O usuário informado não existe");
+		}
+
+	}
+	
 }
