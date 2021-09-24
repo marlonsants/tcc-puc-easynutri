fcbdd5fc120066063e161e41897b995639779da7
marlon <marlutcu@gmail.com>
2021-09-23 22:40:25 -0300

    remoção do da propriedade nome e inclusão do email

--- easynutri-api/src/main/java/com/tcc/easynutri/model/entity/Usuario.java
+++ easynutri-api/src/main/java/com/tcc/easynutri/model/entity/Usuario.java
@@ -1,5 +1,8 @@
 package com.tcc.easynutri.model.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -8,14 +11,19 @@ import javax.persistence.Table;
 @Table(name = "usuario")
 public class Usuario extends EntityBase {
 	
-	private String nome;
-	private String senha;
-	
-	public String getNome() {
-		return nome;
+	public Usuario() {
+		super.setDataCriacao(new Date());
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+		
+	@Column(unique = true)
+	private String email;
+	private String senha;
+		
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public String getSenha() {
 		return senha;
