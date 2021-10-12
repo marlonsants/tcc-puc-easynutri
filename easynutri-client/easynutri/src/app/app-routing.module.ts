f8329ace0fc1577739bbf4f4d442ad9218b454fb
marlon <marlutcu@gmail.com>
2021-10-11 22:39:17 -0300

    Inclusão de rotas filhas para modularizar a aplicação e otimizar o leasing load

--- easynutri-client/easynutri/src/app/app-routing.module.ts
+++ easynutri-client/easynutri/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./pages/login/shared/login.module').then(m => m.LoginModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+]
 })
 export class AppRoutingModule { }
--- easynutri-client/easynutri/src/app/app.module.ts
+++ easynutri-client/easynutri/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +20,12 @@ import { LoginComponent } from './login/login.component';
     TooltipModule.forRoot(),
     ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+        provide: LOCALE_ID,
+        useValue: 'pt-BR',
+    }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
