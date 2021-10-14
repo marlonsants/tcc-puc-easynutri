b868a2475c6773372f637962a764f83d327fdeaa
marlon <marlutcu@gmail.com>
2021-10-13 23:51:52 -0300

    criação do serviço de login

--- easynutri-client/easynutri/src/app/pages/login/shared/login.service.ts
+++ easynutri-client/easynutri/src/app/pages/login/shared/login.service.ts
@@ -1,3 +1,4 @@
+import { Usuario } from './../../../model/usuario.model';
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -7,13 +8,11 @@ import { Observable } from 'rxjs';
 })
 export class LoginService {
 
-  urlBase: string = 'localhost:8080/login';
+  urlBase: string = 'http://localhost:8080/login';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-  }
-
-  login(): Observable<boolean> {
-    return this.http.post<boolean>(this.urlBase,{});
+  login(usuario: Usuario): Observable<boolean> {
+    return this.http.post<boolean>(this.urlBase, JSON.stringify(usuario));
   }
 }
