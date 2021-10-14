dc3fc53e93023650e552e389c568c2f8cbd0d7b0
marlon <marlutcu@gmail.com>
2021-10-13 23:50:15 -0300

    criação de novos componentes

---
+++ easynutri-client/easynutri/src/app/pages/dados-usuario/dados-usuario.module.ts
@@ -0,0 +1,13 @@
+import { DadosUsuarioComponent } from './dados-usuario.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [DadosUsuarioComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DadosUsuarioModule { }
