0231548e0d0eb8f756a83676ac4631b282f60dee
marlon <marlutcu@gmail.com>
2021-10-13 23:51:30 -0300

    remoção do modulo de rotas do componente de login

--- easynutri-client/easynutri/src/app/app-routing.module.ts
+++ easynutri-client/easynutri/src/app/app-routing.module.ts
@@ -1,14 +1,29 @@
+import { DadosUsuarioModule } from './pages/dados-usuario/dados-usuario.module';
+import { DadosUsuarioComponent } from './pages/dados-usuario/dados-usuario.component';
+import { HomeModule } from './pages/home/shared/home.module';
+import { HomeComponent } from './pages/home/home.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginModule } from './pages/login/shared/login.module';
+import { LoginComponent } from './pages/login/login.component';
+
 
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('./pages/login/shared/login.module').then(m => m.LoginModule) }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'cadastro', component: DadosUsuarioComponent }
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    LoginModule,
+    HomeModule,
+    DadosUsuarioModule
+  ],
   exports: [RouterModule],
   providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy }
