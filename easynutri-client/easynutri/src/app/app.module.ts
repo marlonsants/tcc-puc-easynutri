e861d42726dfa713e022f66547f1f06cfa6d509a
marlon <marlutcu@gmail.com>
2021-10-13 23:49:44 -0300

    Implementação de login com conta do goole usando angularx-social-login

--- easynutri-client/easynutri/src/app/app.module.ts
+++ easynutri-client/easynutri/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LOCALE_ID, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
@@ -18,12 +26,30 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     AppRoutingModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    ReactiveFormsModule,
+    SocialLoginModule,
+    HttpClientModule
+
   ],
   providers: [
     {
         provide: LOCALE_ID,
         useValue: 'pt-BR',
+    },
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '998689991370-2rhl1rotnh6dpb7jubcqhhhi7dbh1mda.apps.googleusercontent.com'
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
     }
 ],
   bootstrap: [AppComponent]
--- easynutri-client/easynutri/src/app/pages/login/login.component.ts
+++ easynutri-client/easynutri/src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
+import { LoginService } from './shared/login.service';
 import { Component, OnInit } from '@angular/core';
+
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { Usuario } from './../../model/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  socialUser!: SocialUser;
+  isLoggedin: boolean = false;
+  usuario: Usuario = {};
+
+  constructor(
+    private socialAuthService: SocialAuthService,
+    private loginService: LoginService
+    ) { }
 
   ngOnInit(): void {
+
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = (user != null);
+    });
+
+  }
+
+  login() {
+    this.loginService.login(this.usuario).subscribe((logado: boolean) => {
+      console.log(logado);
+    })
+  }
+
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  logOut(): void {
+    this.socialAuthService.signOut(true);
   }
 
 }
